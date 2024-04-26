439caa185444d590c006cbd1af57d405d300008a
Julie Nguyen <julienguyen2@mail.weber.edu>
2024-04-25 23:51:52 -0600

    finished

--- src/components/BuildList.jsx
+++ src/components/BuildList.jsx
@@ -1,3 +1,7 @@
+// BuildList.jsx
+import { Link } from "react-router-dom";
+import classes from "./BuildList.module.css";
+
 function BuildList({ builds }) {
   return (
     <div className={classes.builds}>
@@ -7,15 +11,13 @@ function BuildList({ builds }) {
           <li key={index} className={classes.item}>
             <Link to={`/build/${index}`}>
               <div className={classes.content}>
-                {build.gpuComponent && <h2>{build.gpuComponent.name}</h2>}
-                {build.cpuComponent && <h2>{build.cpuComponent.name}</h2>}
-                {build.caseComponent && <h2>{build.caseComponent.name}</h2>}
-                {build.mbComponent && <h2>{build.mbComponent.name}</h2>}
-                {build.psComponent && <h2>{build.psComponent.name}</h2>}
-                {build.ramComponent && <h2>{build.ramComponent.name}</h2>}
-                {build.storageComponent && (
-                  <h2>{build.storageComponent.name}</h2>
-                )}
+                <h2>{build.gpuComponent.name}</h2>
+                <h2>{build.cpuComponent.name}</h2>
+                <h2>{build.caseComponent.name}</h2>
+                <h2>{build.mbComponent.name}</h2>
+                <h2>{build.psComponent.name}</h2>
+                <h2>{build.ramComponent.name}</h2>
+                <h2>{build.storageComponent.name}</h2>
               </div>
             </Link>
           </li>
@@ -24,3 +26,5 @@ function BuildList({ builds }) {
     </div>
   );
 }
+
+export default BuildList;
